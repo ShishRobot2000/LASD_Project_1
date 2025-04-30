cfa83e7defd9bea6e7bd545826e1507e0e15f9f7
Pietro <72952819+ShishRobot2000@users.noreply.github.com>
2025-04-30 18:21:41 +0000

    Update traversable.cpp

--- container/traversable.cpp
+++ container/traversable.cpp
@@ -1,18 +1,32 @@
+#include <string> // se usi std::string da qualche parte
+
 namespace lasd {
 
-    /* ************************************************************************** */
-    
-    template <typename Data>
-    bool TraversableContainer<Data>::Exists(const Data& dat) const noexcept {
-      bool found = false;
-      Traverse([&found, &dat](const Data& value) {
-        if (value == dat) {
-          found = true;
-        }
-      });
-      return found;
-    }
-    
-    /* ************************************************************************** */
-    
+/* ************************************************************************** */
+
+template <typename Data>
+bool TraversableContainer<Data>::Exists(const Data& dat) const noexcept {
+  bool found = false;
+  Traverse([&found, &dat](const Data& val) {
+    if (val == dat) found = true;
+  });
+  return found;
+}
+
+/* ************************************************************************** */
+
+template <typename Data>
+void PreOrderTraversableContainer<Data>::Traverse(TraverseFun fun) const {
+  PreOrderTraverse(fun);
+}
+
+/* ************************************************************************** */
+
+template <typename Data>
+void PostOrderTraversableContainer<Data>::Traverse(TraverseFun fun) const {
+  PostOrderTraverse(fun);
+}
+
+/* ************************************************************************** */
+
 }
