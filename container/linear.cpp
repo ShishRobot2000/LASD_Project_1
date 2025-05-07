f604c3fd6e53ab2f095f73edac0943024b8b4210
Pietro <72952819+ShishRobot2000@users.noreply.github.com>
2025-05-07 14:47:38 +0000

    Update linear.cpp

--- container/linear.cpp
+++ container/linear.cpp
@@ -1,22 +1,95 @@
 namespace lasd {
 
-/* ************************************************************************** */
+/* ************************** */
+
+// LinearContainer
 
 template <typename Data>
 void LinearContainer<Data>::Traverse(TraverseFun fun) const {
   PreOrderTraverse(fun);
 }
 
-/* ************************************************************************** */
+template <typename Data>
+bool LinearContainer<Data>::operator==(const LinearContainer& other) const noexcept {
+  if (this->Size() != other.Size())
+    return false;
+  for (unsigned long i = 0; i < this->Size(); ++i) {
+    if ((*this)[i] != other[i])
+      return false;
+  }
+  return true;
+}
+
+template <typename Data>
+bool LinearContainer<Data>::operator!=(const LinearContainer& other) const noexcept {
+  return !(*this == other);
+}
+
+template <typename Data>
+const Data& LinearContainer<Data>::Front() const {
+  if (this->Size() == 0)
+    throw std::length_error("Access to front of an empty container");
+  return (*this)[0];
+}
+
+template <typename Data>
+const Data& LinearContainer<Data>::Back() const {
+  if (this->Size() == 0)
+    throw std::length_error("Access to back of an empty container");
+  return (*this)[this->Size() - 1];
+}
+
+template <typename Data>
+void LinearContainer<Data>::PreOrderTraverse(TraverseFun fun) const {
+  for (unsigned long i = 0; i < this->Size(); ++i)
+    fun((*this)[i]);
+}
+
+template <typename Data>
+void LinearContainer<Data>::PostOrderTraverse(TraverseFun fun) const {
+  for (unsigned long i = this->Size(); i > 0; --i)
+    fun((*this)[i - 1]);
+}
+
+/* ************************** */
+
+// MutableLinearContainer
 
 template <typename Data>
 void MutableLinearContainer<Data>::Map(MapFun fun) {
   PreOrderMap(fun);
 }
 
-/* ************************************************************************** */
+template <typename Data>
+Data& MutableLinearContainer<Data>::Front() {
+  if (this->Size() == 0)
+    throw std::length_error("Access to front of an empty container");
+  return (*this)[0];
+}
 
-// Funzione ausiliaria: merge di due sottosequenze
+template <typename Data>
+Data& MutableLinearContainer<Data>::Back() {
+  if (this->Size() == 0)
+    throw std::length_error("Access to back of an empty container");
+  return (*this)[this->Size() - 1];
+}
+
+template <typename Data>
+void MutableLinearContainer<Data>::PreOrderMap(MapFun fun) {
+  for (unsigned long i = 0; i < this->Size(); ++i)
+    fun((*this)[i]);
+}
+
+template <typename Data>
+void MutableLinearContainer<Data>::PostOrderMap(MapFun fun) {
+  for (unsigned long i = this->Size(); i > 0; --i)
+    fun((*this)[i - 1]);
+}
+
+/* ************************** */
+
+// SortableLinearContainer
+
 template <typename Data>
 void SortableLinearContainer<Data>::Merge(unsigned long left, unsigned long mid, unsigned long right) {
   unsigned long n1 = mid - left + 1;
@@ -47,9 +120,6 @@ void SortableLinearContainer<Data>::Merge(unsigned long left, unsigned long mid,
   }
 }
 
-/* ************************************************************************** */
-
-// Funzione ausiliaria: chiamata ricorsiva di merge sort
 template <typename Data>
 void SortableLinearContainer<Data>::MergeSort(unsigned long left, unsigned long right) {
   if (left < right) {
@@ -60,15 +130,12 @@ void SortableLinearContainer<Data>::MergeSort(unsigned long left, unsigned long 
   }
 }
 
-/* ************************************************************************** */
-
-// Funzione pubblica: chiama merge sort sull'intera sequenza
 template <typename Data>
 void SortableLinearContainer<Data>::Sort() {
   if (this->Size() > 1)
     MergeSort(0, this->Size() - 1);
 }
 
-/* ************************************************************************** */
+/* ************************** */
 
-}
+} // namespace lasd
